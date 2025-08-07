4b5e2b4e73e9bcc31b225b64afb997c05be7b97e
Noleal Arjay <bsemc.nolealarjaym@gmail.com>
2025-08-07 02:46:10 -0700

    changes

--- src/Content.js
+++ src/Content.js
@@ -13,11 +13,7 @@ import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 import services_logo4 from "./assets/images/Services/logo4.png";
 
-import project1 from "./assets/images/Projects/img1.png";
-import project2 from "./assets/images/Projects/img2.png";
-import project3 from "./assets/images/Projects/img3.png";
-import project4 from"./assets/images/Projects/img4.png";
-import person_project from "./assets/images/Projects/person.png";
+
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
